2f1cf296d31af2cbf2b421843b11b4747dfd7421
BJR Matos <bjrmatos@gmail.com>
2018-04-20 14:17:00 -0500

    add optionsSchema

--- jsreport.config.js
+++ jsreport.config.js
@@ -1,6 +1,26 @@
+
 module.exports = {
   'name': 'scripts',
   'main': 'lib/scripts.js',
+  'optionsSchema': {
+    extensions: {
+      scripts: {
+        type: 'object',
+        properties: {
+          timeout: { type: 'number' },
+          allowedModules: {
+            anyOf: [{
+              type: 'string',
+              '$jsreport-constantOrArray': ['*']
+            }, {
+              type: 'array',
+              items: { type: 'string' }
+            }]
+          }
+        }
+      }
+    }
+  },
   'dependencies': ['templates', 'data'],
   'embeddedSupport': true
 }
