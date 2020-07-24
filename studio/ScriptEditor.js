309b0cb0c4c78662011f520a8a8c156f35762838
BJR Matos <bjrmatos@gmail.com>
2020-07-24 17:26:31 -0500

    prop-types is part of standalone package now

--- studio/ScriptEditor.js
+++ studio/ScriptEditor.js
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types'
 import React, { Component } from 'react'
 import { TextEditor } from 'jsreport-studio'
 
@@ -5,16 +6,18 @@ export default class ScriptEditor extends Component {
   render () {
     const { entity, onUpdate } = this.props
 
-    return (<TextEditor
-      name={entity._id}
-      mode='javascript'
-      value={entity.content}
-      onUpdate={(v) => onUpdate(Object.assign({}, entity, {content: v}))}
-    />)
+    return (
+      <TextEditor
+        name={entity._id}
+        mode='javascript'
+        value={entity.content}
+        onUpdate={(v) => onUpdate(Object.assign({}, entity, {content: v}))}
+      />
+    )
   }
 }
 
 ScriptEditor.propTypes = {
-  entity: React.PropTypes.object.isRequired,
-  onUpdate: React.PropTypes.func.isRequired
+  entity: PropTypes.object.isRequired,
+  onUpdate: PropTypes.func.isRequired
 }
